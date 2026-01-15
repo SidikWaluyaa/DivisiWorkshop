05a0799d65768c6ef4db9a242836d882adbf8733
SidikWaluyaa <149220571+SidikWaluyaa@users.noreply.github.com>
2026-01-15 11:19:36 +0700

    Add favicon to tracking index page

--- resources/views/tracking/index.blade.php
+++ resources/views/tracking/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lacak Status & Keluhan - Workshop</title>
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
     
     <!-- Load CSS & JS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
