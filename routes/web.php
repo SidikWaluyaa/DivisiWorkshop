105350c8b1dde0bf1741c8a75740db34e7f0558e
SidikWaluyaa <149220571+SidikWaluyaa@users.noreply.github.com>
2026-02-23 07:32:04 +0700

    fix: change production bulk-update route to POST

--- routes/web.php
+++ routes/web.php
@@ -258,7 +258,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/late-info', [ProductionLateController::class, 'index'])->name('late-info');
         Route::post('/late-info/update-description', [ProductionLateController::class, 'updateDescription'])->name('late-info.update-description');
         Route::post('/late-info/update-new-estimation', [ProductionLateController::class, 'updateNewEstimationDate'])->name('late-info.update-new-estimation');
-        Route::get('/bulk-update', [ProductionController::class, 'bulkUpdate'])->name('bulk-update');
+        Route::post('/bulk-update', [ProductionController::class, 'bulkUpdate'])->name('bulk-update');
     });
 
     // QC
