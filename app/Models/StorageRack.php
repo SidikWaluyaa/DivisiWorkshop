1b81704c87fcf569b10a94a3fe61c7e66e24834d
SidikWaluyaa <149220571+SidikWaluyaa@users.noreply.github.com>
2026-03-03 12:24:10 +0700

    fix: enum cast error on StorageRack status

--- app/Models/StorageRack.php
+++ app/Models/StorageRack.php
@@ -21,7 +21,6 @@ class StorageRack extends Model
 
     protected $casts = [
         'category' => \App\Enums\StorageCategory::class,
-        'status' => \App\Enums\RackStatus::class,
         'capacity' => 'integer',
         'current_count' => 'integer',
     ];
