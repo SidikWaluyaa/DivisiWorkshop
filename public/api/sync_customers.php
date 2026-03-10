c96639ff897e95ba207c1975bdf1c5a3b41ac462
SidikWaluyaa <149220571+SidikWaluyaa@users.noreply.github.com>
2026-03-10 07:31:16 +0700

    Updated sync_customers.php date filter to 2026-02-01

--- public/api/sync_customers.php
+++ public/api/sync_customers.php
@@ -68,7 +68,7 @@ $query = "SELECT
             created_at,
             updated_at
           FROM customers 
-          WHERE created_at >= '2026-02-11 14:15:17'
+          WHERE created_at >= '2026-02-01 00:00:00'
           ORDER BY created_at  ASC";
 
 $result = $mysqli->query($query);
