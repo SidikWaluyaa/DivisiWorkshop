672f1f17c398f151dd9379c197922197cc4e2e9e
SidikWaluyaa <149220571+SidikWaluyaa@users.noreply.github.com>
2026-05-11 14:01:00 +0700

    Fix paths in data remediation script

--- fix_data_flow.php
+++ fix_data_flow.php
@@ -4,8 +4,8 @@ use App\Models\WorkOrder;
 use App\Enums\WorkOrderStatus;
 use Illuminate\Support\Facades\DB;
 
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+require __DIR__ . '/vendor/autoload.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
